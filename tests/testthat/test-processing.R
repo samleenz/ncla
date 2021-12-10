41deeb5a57b1dffc6f92e61cd816a7d36e23d382
samleenz <samleenz@me.com>
2021-12-10 16:40:49 +1100

    starting to write tests for subsetGenes(), tests are incompelte / failing

--- tests/testthat/test-processing.R
+++ tests/testthat/test-processing.R
@@ -1,3 +1,21 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(ncla)
+# make toy data
+ensg <- readRDS(testthat::test_path("ensembl_genes.rds"))
+print(getwd())
+
+df <- matrix(data = pmax(rnorm(1000*25, 100, 50), 0), ncol = 10000, nrow = 25, dimnames = list(LETTERS[1:25], sample(ensg, 10000)))
+
+
+my_geneset <- sample(colnames(df), 1500)
+
+test_that("custom gene-sets work", {
+  expect_error(subsetGenes(df, "Custom__missing_geneset"))
+  expect_equal(ncol(subsetGenes(df, "Custom__my_geneset")), 1500)
+})
+
+test_that("geneset name is valid", {
+  expect_(subsetGenes(df, "Hallmarks")),
+  expect_error(subsetGenes(df, "fail")),
+  expect_equal(subsetGenes(df, "Full"),df),
+  expect_error(subsetGenes(df, "haLlMarKs"))
 })

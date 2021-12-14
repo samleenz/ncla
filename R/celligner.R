7021dbf1d30fd5aea6c52e104ecd6496c5df5099
samleenz <samleenz@me.com>
2021-12-14 16:57:53 +1100

    pondering

---
+++ R/celligner.R
@@ -0,0 +1,12 @@
+run_celligner <- function(x){
+  # load_data
+
+  # make table with ENSG, HGNC, SD, mean for each gene in the tumour and CL data
+  #    is later added to `DE_genes`
+
+  # combine annotation and add tumour / CL designation
+
+  # create 2 seuarat objects
+
+  # cluster tumour and CL separately cluster_data()
+}

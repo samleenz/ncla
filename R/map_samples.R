1f3ae8a42075f94eee50a13d3ce94b7c73adde45
samleenz <samleenz@me.com>
2024-03-01 16:51:27 +1100

    added a map_samples wrapper for SE objects

--- R/map_samples.R
+++ R/map_samples.R
@@ -41,3 +41,32 @@ map_samples <- function(query, reference, f){
     )
   }
 }
+
+#' map_samples_se
+#'
+#' A light wrapper around [map_samples()] for [SummarizedExperiment] objects
+#'
+#' For a given set of `query` samples, perform dissimilarity mapping against the
+#' supplied `reference` using the dissimilarity measure specified by `f`. This
+#' function enforces that the features are the same, e.g. `query = x[, genes]`
+#' and `reference = y[, genes]`.
+#'
+#' @param query row matrix with samples to query
+#' @param reference row matrix with samples to use as the reference
+#' @param f The dissimilarity function to use, see [dissim()]
+#' @param i_q The assay to use from the query SE
+#' @param q_r The assay to use from the reference SE
+#'
+#' @return a data.frame
+#' @export
+map_samples_se <- function(query, reference, f, i_q, i_r){
+  q <- assay(query, i_q) |>
+    t()
+
+  r <- assay(reference, i_r) |>
+    t()
+
+  res <- ncla::map_samples(q, r, f)
+
+  res
+}
